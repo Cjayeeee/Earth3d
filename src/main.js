aea05e2a7ad2c9af120a7cd47442720be197dccd
Jayant Chaudhari <jayanttbnc@gmail.com>
2026-02-06 12:17:04 +0530

    Update texture file path for clouds texture

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load("/earth8k.jpg");
 texture.colorSpace = THREE.SRGBColorSpace;
 
-const texture2 = textureLoader.load("/clouds4k.jpg");
+const texture2 = textureLoader.load("/clouds.jpg");
 texture2.colorSpace = THREE.SRGBColorSpace;
 
 const geometry = new THREE.SphereGeometry(1, 250, 250);
